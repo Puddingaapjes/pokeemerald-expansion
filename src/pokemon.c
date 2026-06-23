80598c428e3fb511a67ff2a4808e8e54ae0a81cc
Puddingaapjes <Smetsjari@hotmail.be>
2026-06-23 22:35:54 +0200

    Base Colour Variants (SpaceOtter99)

--- src/pokemon.c
+++ src/pokemon.c
@@ -54,6 +54,7 @@
 #include "trainer.h"
 #include "trainer_hill.h"
 #include "util.h"
+#include "variant_colours.h"
 #include "constants/abilities.h"
 #include "constants/battle_frontier.h"
 #include "constants/battle_move_effects.h"
@@ -5392,20 +5393,29 @@ const u16 *GetMonFrontSpritePal(struct Pokemon *mon)
 
 const u16 *GetMonSpritePalFromSpeciesAndPersonality(enum Species species, bool32 isShiny, u32 personality)
 {
-    return GetMonSpritePalFromSpeciesIsEgg(species, isShiny, IsPersonalityFemale(species, personality), FALSE);
+    return GetMonSpritePalFromSpeciesIsEgg(species, isShiny, personality, IsPersonalityFemale(species, personality), FALSE);
 }
 
 const u16 *GetMonSpritePalFromSpeciesAndPersonalityIsEgg(enum Species species, bool32 isShiny, u32 personality, bool32 isEgg)
 {
-    return GetMonSpritePalFromSpeciesIsEgg(species, isShiny, IsPersonalityFemale(species, personality), isEgg);
+    return GetMonSpritePalFromSpeciesIsEgg(species, isShiny, personality, IsPersonalityFemale(species, personality), isEgg);
 }
 
 const u16 *GetMonSpritePalFromSpecies(enum Species species, bool32 isShiny, bool32 isFemale)
 {
-    return GetMonSpritePalFromSpeciesIsEgg(species, isShiny, isFemale, FALSE);
+    return GetMonSpritePalFromSpeciesIsEgg(species, isShiny, 0, isFemale, FALSE);
 }
 
-const u16 *GetMonSpritePalFromSpeciesIsEgg(enum Species species, bool32 isShiny, bool32 isFemale, bool32 isEgg)
+const u16 *GetMonSpritePalFromSpeciesIsEgg(enum Species species, bool32 isShiny, u32 personality, bool32 isFemale, bool32 isEgg)
+{
+    const u16 *base = GetMonSpritePalFromSpeciesIsEggInternal(species, isShiny, isFemale, isEgg);
+    static u16 sVariantPal[16];
+    CpuCopy16(base, sVariantPal, sizeof(sVariantPal));
+    ApplyMonSpeciesVariantToPaletteBuffer(species, isShiny, personality, sVariantPal);
+    return sVariantPal;
+}
+
+const u16 *GetMonSpritePalFromSpeciesIsEggInternal(enum Species species, bool32 isShiny, bool32 isFemale, bool32 isEgg)
 {
     species = SanitizeSpeciesId(species);
 
